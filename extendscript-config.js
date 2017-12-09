30e87f159ded4e0f8b1f123b0c88763fa0245ded
Koen Schmeets <k.schmeets@gmail.com>
2017-12-09 21:24:00 +0200

    Add more examples, rename apps to human readable

--- extendscript-config.js
+++ extendscript-config.js
@@ -1,15 +1,39 @@
 // module.exports = {
-//   tsEntry: __dirname + '/src/phsp/tsconfig.json',
-//   jsEntry: __dirname + '/build/phsp/index.js',
-//   dest: '/Applications/Adobe Photoshop CC 2018/Presets/Scripts/ExtendScript Starter.jsx',
+//   tsEntry: __dirname + '/src/AfterEffects/tsconfig.json',
+//   jsEntry: __dirname + '/build/aeft/index.js',
+//   dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
+//   logServerPort: 8081,
+//   devConnectPort: 8080
+// }
+
+// module.exports = {
+//   tsEntry: __dirname + '/src/Audition/tsconfig.json',
+//   jsEntry: __dirname + '/build/audt/index.js',
+//   dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
+//   logServerPort: 8081,
+//   devConnectPort: 8080
+// }
+
+// module.exports = {
+//   tsEntry: __dirname + '/src/Illustrator/tsconfig.json',
+//   jsEntry: __dirname + '/build/ilst/index.js',
+//   dest: '/Applications/Adobe Illustrator CC 2018/Presets.localized/en_GB/Scripts/ExtendScript Starter.jsx',
 //   logServerPort: 8081,
 //   devConnectPort: 8080
 // }
 
 module.exports = {
-  tsEntry: __dirname + '/src/aeft/tsconfig.json',
-  jsEntry: __dirname + '/build/aeft/index.js',
-  dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
+  tsEntry: __dirname + '/src/InDesign/tsconfig.json',
+  jsEntry: __dirname + '/build/idsn/index.js',
+  dest: '/Applications/Adobe InDesign CC 2018/Scripts/Scripts Panel/ExtendScript Starter.jsx',
   logServerPort: 8081,
   devConnectPort: 8080
 }
+
+// module.exports = {
+//   tsEntry: __dirname + '/src/Photoshop/tsconfig.json',
+//   jsEntry: __dirname + '/build/phxs/index.js',
+//   dest: '/Applications/Adobe Photoshop CC 2018/Presets/Scripts/ExtendScript Starter.jsx',
+//   logServerPort: 8081,
+//   devConnectPort: 8080
+// }
