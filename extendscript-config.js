8f1b2dae929abf501ee8cb4617f17db1109cc51c
Koen Schmeets <k.schmeets@gmail.com>
2017-12-11 11:32:01 +0200

    Update types

--- extendscript-config.js
+++ extendscript-config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//   tsEntry: __dirname + '/src/AfterEffects/tsconfig.json',
-//   jsEntry: __dirname + '/build/aeft/index.js',
-//   dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
-//   logServerPort: 8081,
-//   devConnectPort: 8080
-// }
+module.exports = {
+  tsEntry: __dirname + '/src/AfterEffects/tsconfig.json',
+  jsEntry: __dirname + '/build/aeft/index.js',
+  dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
+  logServerPort: 8081,
+  devConnectPort: 8080
+}
 
 // module.exports = {
 //   tsEntry: __dirname + '/src/Audition/tsconfig.json',
@@ -22,13 +22,13 @@
 //   devConnectPort: 8080
 // }
 
-module.exports = {
-  tsEntry: __dirname + '/src/InDesign/tsconfig.json',
-  jsEntry: __dirname + '/build/idsn/index.js',
-  dest: '/Applications/Adobe InDesign CC 2018/Scripts/Scripts Panel/ExtendScript Starter.jsx',
-  logServerPort: 8081,
-  devConnectPort: 8080
-}
+// module.exports = {
+//   tsEntry: __dirname + '/src/InDesign/tsconfig.json',
+//   jsEntry: __dirname + '/build/idsn/index.js',
+//   dest: '/Applications/Adobe InDesign CC 2018/Scripts/Scripts Panel/ExtendScript Starter.jsx',
+//   logServerPort: 8081,
+//   devConnectPort: 8080
+// }
 
 // module.exports = {
 //   tsEntry: __dirname + '/src/Photoshop/tsconfig.json',
